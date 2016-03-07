1e87ef1f8e9c8b97ac5e0006cc63d9bf6ca7ef85
Andrew Hatzistamatis <andy_hatz@hotmail.com>
2016-03-08 10:36:27 +1100

    Initial MVC Project commit

---
+++ ProjectDazza/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectDazza.Startup))]
+namespace ProjectDazza
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
